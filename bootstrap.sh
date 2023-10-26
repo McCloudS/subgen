4d85f39f383ee84ebf3f3312c37bad3fc696e385
McCloudS <64094529+McCloudS@users.noreply.github.com>
2023-10-26 01:09:13 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ pip3 install numpy stable-ts flask faster-whisper requests
 mkdir /subgen
 cd /subgen
 curl https://raw.githubusercontent.com/McCloudS/subgen/main/subgen/subgen.py --output /subgen/subgen.py
-chmod +x /subgen/subgen.py
+chmod 777 /subgen/subgen.py
 
 # Start the program
 
