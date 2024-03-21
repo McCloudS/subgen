2a0031b0cd4ad828ddc03ddc2817414a8630cb6d
McCloudS <64094529+McCloudS@users.noreply.github.com>
2024-03-21 13:55:12 -0600

    Update subgen.py

--- subgen.py
+++ subgen.py
@@ -58,7 +58,7 @@ compute_type = os.getenv('COMPUTE_TYPE', 'auto')
 append = convert_to_bool(os.getenv('APPEND', False))
 reload_script_on_change = convert_to_bool(os.getenv('RELOAD_SCRIPT_ON_CHANGE', False))
 model_prompt = os.getenv('USE_MODEL_PROMPT', 'False')
-custom_model_prompt = os.genenv('CUSTOM_MODEL_PROMPT', '')
+custom_model_prompt = os.getenv('CUSTOM_MODEL_PROMPT', '')
 
 if transcribe_device == "gpu":
     transcribe_device = "cuda"
