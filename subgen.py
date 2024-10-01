ca6b17788905ba0f94127574d8caa3a911f5fa01
McCloudS <64094529+McCloudS@users.noreply.github.com>
2024-09-30 23:07:50 -0600

    Update subgen.py

--- subgen.py
+++ subgen.py
@@ -203,6 +203,10 @@ def has_image_extension(file_path):
 def handle_get_request(request: Request):
     return {"You accessed this request incorrectly via a GET request.  See https://github.com/McCloudS/subgen for proper configuration"}
 
+@app.get("/")
+def webui():
+    return {"The webui for configuration was removed on 1 October 2025, please configure via environment variables or in your Docker settings."}
+
 @app.get("/status")
 def status():
     return {"version" : f"Subgen {subgen_version}, stable-ts {stable_whisper.__version__}, faster-whisper {faster_whisper.__version__} ({docker_status})"}
